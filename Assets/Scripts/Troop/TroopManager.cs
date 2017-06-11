384ff7804e121a4c9d27278126d283a09a6a4c3b
CHLNVgame <CHLNVgame@gmail.com>
2017-06-11 21:27:08 +0700

    - config attribute troop

--- Assets/Scripts/Troop/TroopManager.cs
+++ Assets/Scripts/Troop/TroopManager.cs
@@ -4,7 +4,17 @@ using UnityEngine;
 
 public class TroopManager : MonoBehaviour {
 
+	public int hp;
+	public int speed;
 	public int damge;
+	public int damgeSpec;
+	public float range;
+	public float attackDelay;
+
+	public int foodRequire;
+	public float timeNextTroop;
+	public float ratioSpecSkill;
+
 	public void TakeDamge(int value)
 	{
 		Destroy (gameObject);
